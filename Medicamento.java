3e8b18db51b9a60a884bb5ca2fa6107e6625478f
leticiacpereira <leticia.cpereira@sou.unifeob.edu.br>
2026-09-11 19:31:44 -0300

    Create Medicamento class for medication management

---
+++ Medicamento.java
@@ -0,0 +1,29 @@
+public class Medicamento {
+    private String nome;
+    private String dose;
+    private String horario;
+    private boolean administrado;
+
+    public Medicamento(String nome, String dose, String horario) {
+        this.nome = nome;
+        this.dose = dose;
+        this.horario = horario;
+        this.administrado = false;
+    }
+
+    public void administrar() {
+        this.administrado = true;
+        System.out.println("Medicamento registrado como administrado.");
+    }
+
+    public void mostrar() {
+        System.out.println("Medicamento: " + nome);
+        System.out.println("Dose: " + dose);
+        System.out.println("Horario: " + horario);
+        if (administrado) {
+            System.out.println("Status: Administrado");
+        } else {
+            System.out.println("Status: Pendente");
+        }
+    }
+}
